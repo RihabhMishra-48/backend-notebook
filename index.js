613654821f87efeab62a00cb507e94ef5cc3d094
Rishabh-48 <r72782067@gmail.com>
2025-07-02 09:23:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path= require('path')
+const fs = require('fs')
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine', 'ejs')
+
+app.get('/', function(req,res){
+    fs.readdir(`./files`, function(err, files){
+        res.render("new")
+    })
+    
+})
+
+
+app.listen(3000)
