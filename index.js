03c8cfd74b03539683b658391e91a2baf0c60469
Rishabh-48 <r72782067@gmail.com>
2025-07-10 09:01:25 +0530

    edit feature added

--- index.js
+++ index.js
@@ -14,6 +14,27 @@ app.get('/', function(req,res){
     })
     
 })
+app.get('/files/:filename', function(req, res) {
+    const filePath = path.join(__dirname, 'files', req.params.filename);
+
+    fs.readFile(filePath, 'utf-8', function(err, filedata) {
+        if (err) {
+            console.error("Error reading file:", err);
+            return res.status(404).send("File not found");
+        }
+        res.render('show', { filename: req.params.filename, filedata: filedata });
+    });
+});
+
+app.get('/edit/:filename', function(req, res) {
+    res.render("edit" ,{filename:req.params.filename})
+});
+
+app.post('/edit', function(req, res) {
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}` ,function(err){
+        res.redirect('/')
+    } )
+});
 app.post('/create', function(req,res){
     fs.writeFile(path.join(__dirname, 'files', `${req.body.title.split(' ').join('')}.txt`), req.body.details, function(err){
     if (err) {
